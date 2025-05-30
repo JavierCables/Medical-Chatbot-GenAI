93272a26f5e5c88d1b95647329b59129cb848d80
JavierCables <114525419+JavierCables@users.noreply.github.com>
2025-05-30 16:13:13 +0200

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical Chatbot GenAI",
+    version = "0.0.0",
+    author = "Javier Cables",
+    author_email = "javier.cables00@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
